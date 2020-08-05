e4db6719fae26bf6c54a803d80c45dc3d0d956c8
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-04 17:17:29 -0700

    Update Concatenate_Data.py

--- SoftUni/Python Developmen/Python-Basics/2_simple_calculations/Concatenate_Data.py
+++ SoftUni/Python Developmen/Python-Basics/2_simple_calculations/Concatenate_Data.py
@@ -1,3 +1,10 @@
+"""
+Simple Operations and Calculations - Lab
+02. Concatenate Data
+Write a program that reads the name, surname, age, and city from the console and prints a message of the following type:
+"You are {first_name} {last_name}, a {age} -years old person from {town}."
+"""
+
 first_name = input()
 last_name = input()
 age = input()
