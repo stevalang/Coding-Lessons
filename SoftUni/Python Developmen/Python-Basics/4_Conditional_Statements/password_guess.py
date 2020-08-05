c79c3492256210e1e5eb1d836e67468e67e6da2a
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-05 15:53:49 -0700

    Update password_guess.py

--- SoftUni/Python Developmen/Python-Basics/4_Conditional_Statements/password_guess.py
+++ SoftUni/Python Developmen/Python-Basics/4_Conditional_Statements/password_guess.py
@@ -1,3 +1,16 @@
+"""
+Conditional Statements - Lab
+Check: https://judge.softuni.bg/Contests/Practice/Index/1012#0
+07. Password Guess
+Condition:
+Write a program that reads a password (one line of random text) entered by the user and checks if
+the entry matches the phrase "s3cr3t! P @ ssw0rd". In case of coincidence, display "Welcome".
+In case of discrepancy, display "Wrong password!".
+Sample input and output
+entrance exit entrance exit entrance exit
+qwerty Wrong password! s3cr3t! P @ ssw0rd Welcome s3cr3t! p @ ss Wrong password!
+"""
+
 password = (input())
 
 if password == "s3cr3t!P@ssw0rd":
