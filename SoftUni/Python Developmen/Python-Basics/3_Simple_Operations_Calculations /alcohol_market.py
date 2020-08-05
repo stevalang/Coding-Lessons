344bc2596dfd16b36bcc8ea4881c38f61f307f9f
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-04 17:57:37 -0700

    Update alcohol_market.py

--- SoftUni/Python Developmen/Python-Basics/3_Simple_Operations_Calculations /alcohol_market.py
+++ SoftUni/Python Developmen/Python-Basics/3_Simple_Operations_Calculations /alcohol_market.py
@@ -1,3 +1,25 @@
+"""
+Simple Operations and Calculations - Exercise
+Check: https://judge.softuni.bg/Contests/Compete/Index/1160#0
+07. Alcohol Market
+Condition:
+On foot he decides to have a party and goes to the alcohol exchange to buy beer, wine, brandy and whiskey. To write a program
+which calculates how much money he needs to pay the bill, knowing that:
+the price of brandy is half lower than that of whiskey;
+the price of wine is 40% lower than the price of brandy;
+the price of beer is 80% lower than the price of brandy.
+Entrance
+5 lines are read from the console:
+1. Price of whiskey in BGN - real number;
+2. Quantity of beer in liters - real number;
+3. Quantity of wine in liters - real number;
+4. Quantity of brandy in liters - real number;
+5. Quantity of whiskey in liters - a real number.
+Exit
+Print a number on the console:
+the money that Pesho needs, formatted to the second decimal place.
+"""
+
 price_whiskey = float(input())
 volume_beer_litters = float(input())
 volume_wine_litters = float(input())
