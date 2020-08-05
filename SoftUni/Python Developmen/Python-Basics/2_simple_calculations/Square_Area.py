cbc9429317e73b5886b39afb47ea2b8da0eea99d
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-04 17:21:21 -0700

    Update Square_Area.py

--- SoftUni/Python Developmen/Python-Basics/2_simple_calculations/Square_Area.py
+++ SoftUni/Python Developmen/Python-Basics/2_simple_calculations/Square_Area.py
@@ -1,3 +1,10 @@
+'''
+Simple Operations and Calculations - Lab
+Проверка: https://judge.softuni.bg/Contests/Compete/Index/1011#2
+03. Square Area
+Напишете конзолна програма, която въвежда цяло число 'a' и пресмята лицето на квадрат със страна 'a'.
+'''
+
 a = int(input())
 area = a**2
 print(area)
