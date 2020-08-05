7740053d763b5729fca5fb276c785f343c39b6fa
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-04 17:19:08 -0700

    Update Inches_to_Centimeters.py

--- SoftUni/Python Developmen/Python-Basics/2_simple_calculations/Inches_to_Centimeters.py
+++ SoftUni/Python Developmen/Python-Basics/2_simple_calculations/Inches_to_Centimeters.py
@@ -1,3 +1,10 @@
+"""
+Simple Operations and Calculations - Lab
+Check: https://judge.softuni.bg/Contests/Compete/Index/1011#2
+Write a program that reads a number from the console (not necessarily an integer) and converts the number from inches to centimeters.
+To do this, multiply the inches by 2.54 (because 1 inch = 2.54 centimeters).
+"""
+
 number_sm = float(input())
 number_inch = number_sm * 2.54
 print(number_inch)
