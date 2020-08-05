010794a8fe4b1b949c80800e5cd7801666c48fc8
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-05 16:01:13 -0700

    Update toy_shop.py

--- SoftUni/Python Developmen/Python-Basics/4_Conditional_Statements/toy_shop.py
+++ SoftUni/Python Developmen/Python-Basics/4_Conditional_Statements/toy_shop.py
@@ -1,3 +1,38 @@
+
+'''
+Conditional Statements - Lab
+Check: https://judge.softuni.bg/Contests/Practice/Index/1012#0
+12. Toy Shop
+Condition:
+Petya has a toy store. She receives a large order that she must fulfill. With the money he will earn,
+wants to go on a trip. Write a program that calculates the profit from the order.
+Toy prices:
+-Puzzle - BGN 2.60.
+-Speaking doll - BGN 3.
+- Teddy bear - BGN 4.10.
+-Mignon - BGN 8.20.
+-Truck - BGN 2
+If the ordered toys are 50 or more, the store makes a 25% discount on the total price.
+Petya has to give 10% of the earned money for the rent of the store.
+To calculate whether the money will be enough for her to go on a trip.
+Entrance
+6 lines are read from the console:
+1. Price of the trip - real number;
+2. Number of puzzles - integer;
+3. Number of talking dolls - integer;
+4. Number of teddy bears - an integer;
+5. Number of minions - an integer;
+6. Number of trucks - an integer.
+Exit
+The following is printed on the console:
+-If the money is enough it is printed:
+- "Yes! {Remaining money} lv left."
+-If the money is NOT enough, print:
+- "Not enough money! {Lv needed} lv needed."
+The result must be formatted to the second decimal place.
+'''
+
+
 trip = float(input())
 puzzle_count = int(input())
 doll_count = int(input())
