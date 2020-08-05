362298b2ba9bdcecdcfd3caa22f743d8c754132c
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-04 17:20:00 -0700

    Update Projects_Creation.py

--- SoftUni/Python Developmen/Python-Basics/2_simple_calculations/Projects_Creation.py
+++ SoftUni/Python Developmen/Python-Basics/2_simple_calculations/Projects_Creation.py
@@ -1,3 +1,19 @@
+"""
+Simple Operations and Calculations - Lab
+05. Creation Projects
+Check: https://judge.softuni.bg/Contests/Compete/Index/1011#2
+Write a program that calculates how many hours it will take an architect to design several
+construction sites. The preparation of a project takes approximately three hours.
+Entrance
+2 lines are read from the console:
+1. The name of the architect - text;
+2. Number of projects - integer.
+Exit
+The following is printed on the console:
+"The architect {architect's name} will need {hours needed} hours to complete {number of projects} project / s."
+"""
+
+
 name = input()
 projects_count = int(input())
 time_needed = int(projects_count*3)
