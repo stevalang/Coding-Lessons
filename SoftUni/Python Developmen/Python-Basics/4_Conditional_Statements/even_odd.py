0102232eb80235b044f7b825ef7c26c11d9bf861
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-05 15:51:13 -0700

    Update even_odd.py

--- SoftUni/Python Developmen/Python-Basics/4_Conditional_Statements/even_odd.py
+++ SoftUni/Python Developmen/Python-Basics/4_Conditional_Statements/even_odd.py
@@ -1,3 +1,17 @@
+"""
+Conditional Statements - Lab
+Check: https://judge.softuni.bg/Contests/Practice/Index/1012#0
+03. Even or Odd
+Condition:
+Write a program that reads an integer entered by the user and prints it to the console
+whether the number is even or odd.
+1. First create a Python file with an appropriate name in the existing project;
+2. Read an integer from the console:
+3. Check that the number is even by dividing it modularly by 2 and check that there is a remainder of the division.
+If there is no residue, print an "even" output. Otherwise, print "odd":
+"""
+
+
 number = int(input())
 
 if number % 2 == 0:
