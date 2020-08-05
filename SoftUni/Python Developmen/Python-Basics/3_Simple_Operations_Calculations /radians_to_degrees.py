a5226909b870a005c6716733925a29ef5ea9b841
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-04 18:03:23 -0700

    Update radians_to_degrees.py

--- SoftUni/Python Developmen/Python-Basics/3_Simple_Operations_Calculations /radians_to_degrees.py
+++ SoftUni/Python Developmen/Python-Basics/3_Simple_Operations_Calculations /radians_to_degrees.py
@@ -1,22 +1,13 @@
 """
 Simple Operations and Calculations - Exercise
+02. Radians to Degrees
 Check: https://judge.softuni.bg/Contests/Compete/Index/1160#0
-05. Dance Hall
 Condition:
-A group of dancers are looking for a new hall. The hall they liked is rectangular and has dimensions:
-L - length and W - width (in meters). The hall has a square wardrobe with a side - A
-and a rectangular bench with an area 10 times smaller than the area of ​​the hall.
-The place occupied by a dancer is 40 cm² and in addition he needs another 7000 cm² for free movement.
-Write a program that calculates how many dancers can fit in the hall and move freely.
-The result obtained should be rounded down to the nearest whole number.
-Entrance
-3 lines are read from the console:
-1. L - length of the hall in meters - real number;
-2. W - width of the hall in meters - real number;
-3. A - side of the wardrobe in meters - a real number.
-Exit
-Print an integer on the console - the number of dancers that can fit in the free space
-of the hall, rounded to the nearest whole number down.
+Write a program that reads an angle in radians (rad) and converts it to degrees (deg).
+Round the result to the nearest whole number.
+Use the formula: degree = radian * 180 / π.
+The number π in Python can be accessed through the math module. To use its functionality,
+you must first import the module.
 """
 
 
