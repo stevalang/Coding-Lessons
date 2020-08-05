6595f956338b9c0146814a4ab3565b0910fa79af
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-04 17:18:26 -0700

    Update Greeting_by_Name.py

--- SoftUni/Python Developmen/Python-Basics/2_simple_calculations/Greeting_by_Name.py
+++ SoftUni/Python Developmen/Python-Basics/2_simple_calculations/Greeting_by_Name.py
@@ -1,2 +1,9 @@
+"""
+Simple Operations and Calculations - Lab
+01. Greeting by Name
+Write a program that reads a person's name from the console and prints
+"Hello, {name}!", Where {name} is the name entered from the console.
+"""
+
 name = input()
 print(f'Hello,{name}!')
