55d75e097ef9dae0d4dea35615af7b415a77818d
Stefan Angelov <rbeecommerce@gmail.com>
2020-08-07 05:44:21 -0700

    upload

---
+++ The-Python-Planner/skill1.py
@@ -0,0 +1,21 @@
+# PRINT
+# You can use the hash sign (#) to comment out code or write notes
+# in the code that are not read as part of the program.
+
+# Exercise 1
+
+# Type print('Hello, World') or print("Hello, World") in the console to the right and 
+# hit enter.
+
+# Exercise 2
+
+# Highlight the first 2 lines and either click the Run Selection button or F9.
+
+print('Hello')
+print('Hi')
+print('Hello, World')
+
+# Exercise 3 
+
+print('She says "Hello, World."')
+
