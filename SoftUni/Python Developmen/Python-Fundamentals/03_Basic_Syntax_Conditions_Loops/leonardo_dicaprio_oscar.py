e007c397b28d3cf35fd82d4a2b9b7614a6a53b08
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-12 16:47:41 -0700

    Update leonardo_dicaprio_oscar.py

--- SoftUni/Python Developmen/Python-Fundamentals/03_Basic_Syntax_Conditions_Loops/leonardo_dicaprio_oscar.py
+++ SoftUni/Python Developmen/Python-Fundamentals/03_Basic_Syntax_Conditions_Loops/leonardo_dicaprio_oscar.py
@@ -1,3 +1,12 @@
+'''
+Write a program that receives a single integer number and prints different messages depending on the number:
+If Oscar is 88 - "Leo finally won the Oscar! Leo is happy".
+If Oscar is 86 - "Not even for Wolf of Wall Street?!"
+If Oscar is not 88 nor 86 (and below 88) - "When will you give Leo an Oscar?"
+If Oscar is over 88 - "Leo got one already!"
+'''
+
+
 line = int(input())
 if line == 88:
     print("Leo finally won the Oscar! Leo is happy")
