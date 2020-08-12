e15768649114d5e121b5e0d043b28c5cff920416
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-12 16:44:24 -0700

    Update jenny_secret_message.py

--- SoftUni/Python Developmen/Python-Fundamentals/03_Basic_Syntax_Conditions_Loops/jenny_secret_message.py
+++ SoftUni/Python Developmen/Python-Fundamentals/03_Basic_Syntax_Conditions_Loops/jenny_secret_message.py
@@ -1,3 +1,8 @@
+'''
+Jenny studies programming with python and wants to create a program that greets a user when he/she gives his/her name. Jenny however is in love with Johnny, and would like to greet him differently. Can you help her?
+'''
+
+
 name = input()
 
 if name == 'Johnny':
