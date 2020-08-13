b02c5298a5028e880e65bc777428da438f992fb8
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-13 09:46:59 -0700

    Update next_happy_year.py

--- SoftUni/Python Developmen/Python-Fundamentals/03_Basic_Syntax_Conditions_Loops/next_happy_year.py
+++ SoftUni/Python Developmen/Python-Fundamentals/03_Basic_Syntax_Conditions_Loops/next_happy_year.py
@@ -1,3 +1,7 @@
+'''
+You're saying good-bye your best friend, "See you next happy year". Happy Year is the year with only distinct digits, (e.g) 2018. Write a program that receives an integer number and finds the next happy year.
+'''
+
 year = input()
 year_as_int = int(year) + 1
 year = str(year_as_int)
