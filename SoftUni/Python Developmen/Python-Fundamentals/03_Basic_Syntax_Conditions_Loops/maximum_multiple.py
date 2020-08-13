05ec133e2130c9ec530bb5ea6c55e72bf4fbd3f6
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-13 09:47:30 -0700

    Update maximum_multiple.py

--- SoftUni/Python Developmen/Python-Fundamentals/03_Basic_Syntax_Conditions_Loops/maximum_multiple.py
+++ SoftUni/Python Developmen/Python-Fundamentals/03_Basic_Syntax_Conditions_Loops/maximum_multiple.py
@@ -1,3 +1,12 @@
+'''
+Given a Divisor and a Bound, find the largest integer N, such that:
+N is divisible by divisor
+N is less than or equal to bound
+N is greater than 0.
+Notes: The divisor and bound are only positive values. It's guaranteed that a divisor is found
+'''
+
+
 divisor = int(input())
 bound = int(input())
 max_num = 0
