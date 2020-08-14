b3ef080e96edb3ed1bd4a84ce7a24d94c8546411
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-14 08:25:10 -0700

    Update mutate_string.py

--- SoftUni/Python Developmen/Python-Fundamentals/03_Basic_Syntax_Conditions_Loops/mutate_string.py
+++ SoftUni/Python Developmen/Python-Fundamentals/03_Basic_Syntax_Conditions_Loops/mutate_string.py
@@ -1,3 +1,8 @@
+'''
+You will be given two strings. Transform the first string into the second one, one letter at a time and print it. Print only the unique strings
+Note: the strings will have the same lengths
+'''
+
 str_one = input()
 str_two = input()
 
