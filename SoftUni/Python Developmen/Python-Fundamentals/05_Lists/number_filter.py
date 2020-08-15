4f9e6b81dca31d368697551f823fa8a143227788
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-15 07:50:45 -0700

    Update number_filter.py

--- SoftUni/Python Developmen/Python-Fundamentals/05_Lists/number_filter.py
+++ SoftUni/Python Developmen/Python-Fundamentals/05_Lists/number_filter.py
@@ -1,3 +1,36 @@
+"""
+Lists Basics - Lab
+Check your code: https://judge.softuni.bg/Contests/Practice/Index/1724#4
+SUPyF2 Lists Basics Lab - 05. Numbers Filter
+Problem:
+You will receive a single number n. On the next n lines you will receive integers.
+After that you will be given one of the following commands:
+•	even
+•	odd
+•	negative
+•	positive
+Filter all the numbers that fit in the category (0 counts as a positive). Finally, print the result.
+Example:
+Input:
+5
+33
+19
+-2
+18
+998
+even
+Output:
+[19, -2, 18, 998]
+Input:
+3
+111
+-4
+0
+negative
+Output:
+[-4]
+"""
+
 n = int(input())
 
 numbers = []
