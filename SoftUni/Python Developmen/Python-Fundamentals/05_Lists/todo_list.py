83d9d25839850cb899a1687b54f0267306aa34ea
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-15 07:48:45 -0700

    Update todo_list.py

--- SoftUni/Python Developmen/Python-Fundamentals/05_Lists/todo_list.py
+++ SoftUni/Python Developmen/Python-Fundamentals/05_Lists/todo_list.py
@@ -1,3 +1,27 @@
+"""
+Lists Basics - Lab
+Check your code: https://judge.softuni.bg/Contests/Practice/Index/1724#2
+SUPyF2 Lists Basics Lab - 03. Lists Statistics
+Problem:
+You will be given a number n. On the next n lines you will receive integers. You have to create and print two lists:
+•	One with all the positives (including 0)
+•	One with all the negatives
+Finally print the following message: "Count of positives: {count_positives}. Sum of negatives: {sum_of_negatives}"
+Example:
+Input:
+5
+10
+3
+2
+-15
+-4
+Output:
+[10, 3, 2]
+[-15, -4]
+Count of positives: 3. Sum of negatives: -19
+"""
+
+
 notes = []
 while True:
     string = input()
