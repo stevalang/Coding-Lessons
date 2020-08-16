3bfceb9e6bc8f44b3fab888ba933ad9f14d6a6c5
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-16 08:29:00 -0700

    Update multiples_list.py

--- SoftUni/Python Developmen/Python-Fundamentals/05_Lists/multiples_list.py
+++ SoftUni/Python Developmen/Python-Fundamentals/05_Lists/multiples_list.py
@@ -1,3 +1,8 @@
+"""
+Write a program that receives two numbers (factor and count) and creates a list with length of the given count and contains only elements that are multiples of the given factor
+
+"""
+
 factor = int(input())
 count = int(input())
 nums = []
