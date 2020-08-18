a26adbacf999f76db5116f9bbbe79a8642786ba8
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-18 16:43:34 -0700

    Update invert_values.py

--- SoftUni/Python Developmen/Python-Fundamentals/05_Lists/invert_values.py
+++ SoftUni/Python Developmen/Python-Fundamentals/05_Lists/invert_values.py
@@ -1,3 +1,7 @@
+'''
+Write a program that receives a single string containing numbers separated by a single space. Print a list containing the opposite of each number
+'''
+
 string_number = input().split()
 opposite = []
 for num in string_number:
