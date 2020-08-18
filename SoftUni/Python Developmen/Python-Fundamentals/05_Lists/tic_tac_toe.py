831f04848c0cf51826c78942dc6c241a5b4af698
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-18 16:47:58 -0700

    Update tic_tac_toe.py

--- SoftUni/Python Developmen/Python-Fundamentals/05_Lists/tic_tac_toe.py
+++ SoftUni/Python Developmen/Python-Fundamentals/05_Lists/tic_tac_toe.py
@@ -1,3 +1,12 @@
+"""
+You will receive a field of a tic-tac-toe game in three lines containing numbers separated by a single space.
+Legend:
+0 - empty space
+1 - first player move
+2 - second player move
+Find out who the winner is. If the first player wins print "First player won", otherwise if the second player wins print "Second player won", otherwise print "Draw!"
+"""
+
 import numpy as np
 import random
 from time import sleep
