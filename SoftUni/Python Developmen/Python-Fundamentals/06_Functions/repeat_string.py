b358d054f7f07d91c9789fd4e8e2d965a8eeeb08
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-20 09:02:28 -0700

    Update repeat_string.py

--- SoftUni/Python Developmen/Python-Fundamentals/06_Functions/repeat_string.py
+++ SoftUni/Python Developmen/Python-Fundamentals/06_Functions/repeat_string.py
@@ -1,3 +1,7 @@
+"""
+Write a function that receives a string and a repeat count n. The function should return a new string (the old one repeated n times).
+"""
+
 def string_repeat(text, count):
     result = text * count
     return result
