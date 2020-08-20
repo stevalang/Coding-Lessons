5528e0890429c149507d222a3e9528e3a799aaf4
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-19 18:54:39 -0700

    Update add_subtract.py

--- SoftUni/Python Developmen/Python-Fundamentals/06_Functions/add_subtract.py
+++ SoftUni/Python Developmen/Python-Fundamentals/06_Functions/add_subtract.py
@@ -1,3 +1,10 @@
+"""
+
+You will receive three integer numbers. 
+Write a function sum_numbers() to get the sum of the first two integers and subtract() function that subtracts the third integer from the result. Wrap the two functions in a function called add_and_subtract() which will receive the three numbers
+
+"""
+
 # def sum_numbers(a, b, c):
 #     return a + b - c
 #
