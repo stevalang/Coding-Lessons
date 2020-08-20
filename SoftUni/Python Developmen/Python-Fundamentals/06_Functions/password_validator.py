62c1829c7e1cb317537e593fcff87e54e9eccc60
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-19 18:56:20 -0700

    Update password_validator.py

--- SoftUni/Python Developmen/Python-Fundamentals/06_Functions/password_validator.py
+++ SoftUni/Python Developmen/Python-Fundamentals/06_Functions/password_validator.py
@@ -1,3 +1,16 @@
+"""
+Write a function that checks if a given password is valid. Password validations are:
+The length should be 6 - 10 characters (inclusive)
+It should consists only letters and digits
+It should have at least 2 digits 
+If a password is valid print "Password is valid".
+If it is NOT valid, for every unfulfilled rule print a message:
+"Password must be between 6 and 10 characters"
+"Password must consist only of letters and digits"
+"Password must have at least 2 digits"
+"""
+
+
 # def in_range(password):
 #     if 6 > len(password) > 10:
 #         return "Password must be between 6 and 10 characters"
