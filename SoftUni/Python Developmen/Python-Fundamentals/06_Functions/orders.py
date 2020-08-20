2537943c4de05ea9622a07f5c209a4ff0ae0da47
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-20 09:02:55 -0700

    Update orders.py

--- SoftUni/Python Developmen/Python-Fundamentals/06_Functions/orders.py
+++ SoftUni/Python Developmen/Python-Fundamentals/06_Functions/orders.py
@@ -1,3 +1,7 @@
+"""
+Write a function that calculates the total price of an order and prints it on the console. The function should receive one of the following products: coffee, coke, water, snacks; and a quantity of the product. The prices for a single piece of each product are: 
+"""
+
 def find_price_per_unit(product, quantity):
     if product == 'coffee':
         return 1.5 * quantity
