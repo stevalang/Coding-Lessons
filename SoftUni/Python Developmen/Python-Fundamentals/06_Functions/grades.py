8ba318029dd017ce47a82d0fb050eb8a963bb35f
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-20 09:01:08 -0700

    Update grades.py

--- SoftUni/Python Developmen/Python-Fundamentals/06_Functions/grades.py
+++ SoftUni/Python Developmen/Python-Fundamentals/06_Functions/grades.py
@@ -1,3 +1,14 @@
+"""
+
+Write a function that receives a grade between 2.00 and 6.00 and prints the corresponding grade in words
+2.00 – 2.99 - "Fail"
+3.00 – 3.49 - "Poor"
+3.50 – 4.49 - "Good"
+4.50 – 5.49 - "Very Good"
+5.50 – 6.00 - "Excellent"
+
+"""
+
 def is_fail(grade):
     return 2.00 <= grade <= 2.99
 
