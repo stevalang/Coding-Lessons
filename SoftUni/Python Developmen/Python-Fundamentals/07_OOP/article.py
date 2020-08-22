38c5375a401a83b85e9d01d0d246f22df2539428
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-22 09:44:14 -0700

    Update article.py

--- SoftUni/Python Developmen/Python-Fundamentals/07_OOP/article.py
+++ SoftUni/Python Developmen/Python-Fundamentals/07_OOP/article.py
@@ -1,3 +1,13 @@
+"""
+
+Create a class Article. The __init__ method should accept 3 arguments: title, content, author. The class should also have 4 methods:
+edit(new_content) - changes the old content to the new one
+change_author(new_author) - changes the old author to with the new one
+rename(new_title) - changes the old title with the new one
+__repr__() - returns the following string "{title} - {content}: {author}"
+
+"""
+
 class Article:
 
     def __init__(self, title, content, author):
