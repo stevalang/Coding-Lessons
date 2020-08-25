d6562c67bfaa063bb2a5b14d4d0972c0c39f0589
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-25 06:09:23 -0700

    Update a_miner_task.py

--- SoftUni/Python Developmen/Python-Fundamentals/07_Dictionaries/a_miner_task.py
+++ SoftUni/Python Developmen/Python-Fundamentals/07_Dictionaries/a_miner_task.py
@@ -1,3 +1,10 @@
+"""
+You will be given a sequence of strings, each on a new line. Every odd line on the console is representing a resource (e.g. Gold, Silver, Copper, and so on) and every even – quantity. Your task is to collect the resources and print them each on a new line.
+Print the resources and their quantities in the following format:
+{resource} –> {quantity}
+The quantities will be in the range [1 … 2 000 000 000]
+"""
+
 resource = input()
 
 mine = {}
