b8ba7413d8af9416049f1016affdc19acda706b8
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-25 06:12:16 -0700

    Update company_users.py

--- SoftUni/Python Developmen/Python-Fundamentals/07_Dictionaries/company_users.py
+++ SoftUni/Python Developmen/Python-Fundamentals/07_Dictionaries/company_users.py
@@ -1,3 +1,17 @@
+"""
+Write a program that keeps information about companies and their employees. 
+You will be receiving a company name and an employee's id, until you receive the command "End" command. Add each employee to the given company. Keep in mind that a company cannot have two employees with the same id.
+When you finish reading the data, order the companies by the name in ascending order. 
+Print the company name and each employee's id in the following format:
+{companyName}
+-- {id1}
+-- {id2}
+-- {idN}
+Input / Constraints
+Until you receive the "End" command, you will be receiving input in the format: "{companyName} -> {employeeId}".
+The input always will be valid.
+"""
+
 employees = {}
 
 while True:
