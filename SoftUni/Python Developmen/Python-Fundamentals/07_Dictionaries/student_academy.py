6492f8f21e60c2058c206b9dcd8103032260989f
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-25 06:11:53 -0700

    Update student_academy.py

--- SoftUni/Python Developmen/Python-Fundamentals/07_Dictionaries/student_academy.py
+++ SoftUni/Python Developmen/Python-Fundamentals/07_Dictionaries/student_academy.py
@@ -1,4 +1,11 @@
-
+"""
+Write a program that keeps information about students and their grades. 
+You will receive n pair of rows. First you will receive the student's name, after that you will receive his grade. Check if the student already exists and if not, add him. Keep track of all grades for each student.
+When you finish reading the data, keep the students with average grade higher than or equal to 4.50. Order the filtered students by average grade in descending order.
+Print the students and their average grade in the following format:
+{name} –> {averageGrade}
+Format the average grade to the 2nd decimal place.
+"""
 
 n = int(input())
 
