ca0e476f197403cb3e78f1d11012ba34111a016e
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-25 06:11:25 -0700

    Update courses.py

--- SoftUni/Python Developmen/Python-Fundamentals/07_Dictionaries/courses.py
+++ SoftUni/Python Developmen/Python-Fundamentals/07_Dictionaries/courses.py
@@ -1,3 +1,15 @@
+"""
+Write a program that keeps information about courses. Each course has a name and registered students.
+You will be receiving a course name and a student name, until you receive the command "end". Check if such course already exists, and if not, add the course. Register the user into the course. When you receive the command "end", print the courses with their names and total registered users, ordered by the count of registered users in descending order. For each contest print the registered users ordered by name in ascending order.
+Input
+Until the "end" command is received, you will be receiving input in the format: "{courseName} : {studentName}".
+The product data is always delimited by " : ".
+Output
+Print the information about each course in the following the format:  "{courseName}: {registeredStudents}"
+Print the information about each student, in the following the format: "-- {studentName}"
+"""
+
+
 courses = {}
 
 while True:
