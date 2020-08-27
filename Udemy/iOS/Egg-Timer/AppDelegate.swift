25fd71b9c758ac04a998ddf34d011acf717544f1
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-26 20:11:14 -0700

    Update AppDelegate.swift

--- Udemy/iOS/Egg-Timer/AppDelegate.swift
+++ Udemy/iOS/Egg-Timer/AppDelegate.swift
@@ -1,3 +1,5 @@
+// You will need to apply everything you have learnt before to complete this challenge. But most importantly, we want you to get confortable with looking up how to do something you've never done before. Follow the 5 step process you learnt in the Xylophone module and build a beautiful egg timer app to make yourself proud. 
+
 import UIKit
 
 @UIApplicationMain
