3fd4338036c53f2674f702760423c259d485baf6
Stefan Angelov <rbeecommerce@gmail.com>
2020-08-26 20:01:49 -0700

    upload

---
+++ Swift/optionalsExercise.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+//var player1Username: String = nil
+var player1Username: String? = nil
+
+
+//var unwrappedP1Username = player1Username!
+player1Username = "jack"
+
+player1Username = nil
+
+if player1Username != nil{
+    print(player1Username!)
+}
+
