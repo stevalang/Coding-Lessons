9c32003b396a24d5eaf103d35525a94c46c87d8a
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-26 20:15:47 -0700

    Update ViewController.swift

--- Udemy/iOS/Magic-8-Ball/ViewController.swift
+++ Udemy/iOS/Magic-8-Ball/ViewController.swift
@@ -1,10 +1,6 @@
-//
-//  ViewController.swift
-//  Magic 8 Ball
-//
-//  Created by Angela Yu on 14/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
+// The objective of this challenge is to apply the skills you learned in the Dicee tutorial and get you making an app with functionality all by yourself. There’s no new concepts here. But you’re flyin’ solo this time! Most of the programming skills are gained in the challenges rather than the tutorials. It’s when you’re using your new-found programming skills to bend the app to your will that you’re truly levelling up as a developer.
+// What you will create
+// We’re going to make a Magic 8 Ball app. You can ask the app to make all your hard decisions! With this app in your pocket, you’ll always have an answer to life’s many conundrums!
 
 import UIKit
 
