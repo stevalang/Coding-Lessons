3d6a0c4f27758070ffbe6138cd02257aba3c0f99
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-27 08:35:51 -0700

    Update ViewController.swift

--- Udemy/iOS/Xylophone/ViewController.swift
+++ Udemy/iOS/Xylophone/ViewController.swift
@@ -1,3 +1,5 @@
+// You will be making your first musical instrument! Music apps are so popular on the App Store that they even get their own category. So in this module, we’re going to make a colourful XyloPhone app. Get it? Ok, the jokes are bad, but remember, I only wrote the good ones... 
+
 import UIKit
 import AVFoundation
 
