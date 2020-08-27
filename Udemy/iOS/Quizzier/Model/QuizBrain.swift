3bb18db479a79eaa4a741cccf17042d74d5f2889
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-26 20:21:50 -0700

    Update QuizBrain.swift

--- Udemy/iOS/Quizzier/Model/QuizBrain.swift
+++ Udemy/iOS/Quizzier/Model/QuizBrain.swift
@@ -1,3 +1,6 @@
+// The goal of this tutorial is to take you one step further in your journey of becoming an app developer. We are going to introduce you to the holy grail of mobile design patterns: the Model View Controller (MVC) pattern. A design pattern is simply a repeatable and optimised solution to a common software problem and you’ll learn more about this when use in the tutorial.
+
+
 import Foundation
 
 struct QuizBrain {
