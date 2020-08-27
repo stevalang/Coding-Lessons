8a87118502cc52c10a5f878b856b27daeb653c18
Stefan Angelov <stefan.valentinov.angelov@gmail.com>
2020-08-26 20:22:28 -0700

    Update Question.swift

--- Udemy/iOS/Quizzier/Model/Question.swift
+++ Udemy/iOS/Quizzier/Model/Question.swift
@@ -1,3 +1,5 @@
+// You will program a trivia quiz app. If you have another field of expertise such as law or medicine, you’ll be glad to know that multiple choice question apps are one of the most popular types of educational apps on the App Store! 
+
 import Foundation
 
 struct Question {
